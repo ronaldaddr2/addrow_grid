ae08575d78a24d440291278d9698c4a680fabbd0
Ronald adriansyah <sschouse@gmail.com>
2019-09-30 00:18:30 +0700

    Add files via upload

---
+++ View/trx_grid.php
@@ -0,0 +1,12 @@
+
+
+<script src="<?php echo base_url() ?>assets/js/vendor/jquery-1.11.2.min.js"></script>
+<script src="<?php echo base_url() ?>assets/js/fungsi.js"></script> 
+<body>
+<h2>EDITABLE GRID</h2>
+<span><div id="countrow">0</div></span><span><div id="labelcount">baris</div></span>
+<button type="button" class="btnbatal" ID="CMD1" ONCLICK="addrows();">ADD NEW</button>
+<table BORDER="1">
+</table>
+</body>
+<div id="addnewgrid"></div> 
